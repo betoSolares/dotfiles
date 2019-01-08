258a443beb8fe4f37956250d0aae09b4412a5ae9
Roberto Solares <betosolaresgar@gmail.com>
2019-01-08 16:48:59 -0600

    Add the new keybandings

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -25,45 +25,120 @@
 # SOFTWARE.
 
 # Imports
-from libqtile.config import Key
+from libqtile.config import Key, Screen, Group, Drag, Click
 from libqtile.command import lazy
+from libqtile import layout, bar, widget
 
-# Personal Functions
-@lazy.function
-def prev_group(qtile):
-    if qtile.currentWindow is not None:
-        i = qtile.groups.index(qtile.currentGroup)
-        qtile.currentWindow.togroup(qtile.groups[i - 1].name
-
-@lazy.function
-def next_group(qtile):
-    if qtile.currentWindow is not None:
-        i = qtile.groups.index(qtile.currentGroup)
-        qtile.currentWindow.togroup(qtile.groups[i + 1].name
+mod = "mod4"
 
 # Keybandings
-def init_keys():
-    return [
-        # Applications
-        Key([mod], "Return", lazy.spawn("st")),
-        Key([mod], "c", lazy.spawn("chromium")),
-        Key([mod, "control"], "c", lazy.spawn("chromium --incognito")),
-        Key([mod], "d", lazy.spawn("dmenu_run")),
+keys = [
+    # Applications
+    Key([mod], "Return", lazy.spawn("st")),
+    Key([mod], "v", lazy.spawn("vivaldi-stable")),
+    Key([mod, "control"], "v", lazy.spawn("vivaldi-stable --incognito")), 
+    Key([mod], "d", lazy.spawn("dmenu_run -b")), 
 
-        # Window Control
-        Key([mod], "k", lazy.layout.down()),
-        Key([mod], "j", lazy.layout.up()),
-        Key([mod], "Tab", lazy.next_layout()),
-        Key([mod, "shift"], "Tab", lazy.prev_layout()),
-        Key([mod], "Left", prev_group),
-        Key([mod], "Right", next_group),
-        Key([mod], "l", lazy.layout.increase_ratio()),
-        Key([mod], "h", lazy.layout.decrase_ratio()),
-        Key([mod, "shift"], "c", lazy.window.kill()),
-        Key([mod], "space", lazy.layout.next()),
-        Key([mod, "shift"], "space", lazy.layout.toggle_floating()),
+    # Audio Control
+    Key([], "XF86AudioLowerVolume", lazy.spawn("amixer -c 1 -q set Master 1.75dB-")),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer -c 1 -q set Master 1.75dB+")),
+    Key([], "XF86AudioMute", lazy.spawn("amixer -c 1 -q set Master toggle")),
 
-        # Restart Qtile
-        Key([mod, "shift"], "r", lazy.restart()),
-    ]
+    # Window Controls
+    Key([mod], "k", lazy.layout.down()),
+    Key([mod], "j", lazy.layout.up()),
+    Key([mod], "l", lazy.layout.increase_ratio()),
+    Key([mod], "h", lazy.layout.decrease_ratio()),
+    Key([mod], "Tab", lazy.next_layout()),
+    Key([mod], "Left", lazy.screen.prev_group()),
+    Key([mod], "Right", lazy.screen.next_group()),
+    Key([mod, "shift"], "h", lazy.screen.prev_group()), 
+    Key([mod, "shift"], "l", lazy.screen.next_group()), 
+    Key([mod, "shift"], "Tab", lazy.prev_layout()),
+    Key([mod], "b", lazy.hide_show_bar()), 
+    Key([mod, "shift"], "c", lazy.window.kill()),
+    Key([mod, "shift"], "r", lazy.restart()),
+]
 
+groups = [Group(i) for i in "asdfuiop"]
+
+for i in groups:
+    keys.extend([
+        # mod1 + letter of group = switch to group
+        Key([mod], i.name, lazy.group[i.name].toscreen()),
+
+        # mod1 + shift + letter of group = switch to & move focused window to group
+        Key([mod, "shift"], i.name, lazy.window.togroup(i.name)),
+    ])
+
+layouts = [
+    layout.Max(),
+    layout.Stack(num_stacks=2)
+]
+
+widget_defaults = dict(
+    font='sans',
+    fontsize=12,
+    padding=3,
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        bottom=bar.Bar(
+            [
+                widget.GroupBox(),
+                widget.Prompt(),
+                widget.WindowName(),
+                widget.TextBox("default config", name="default"),
+                widget.Systray(),
+                widget.Clock(format='%Y-%m-%d %a %I:%M %p'),
+            ],
+            24,
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+main = None
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    {'wmclass': 'confirm'},
+    {'wmclass': 'dialog'},
+    {'wmclass': 'download'},
+    {'wmclass': 'error'},
+    {'wmclass': 'file_progress'},
+    {'wmclass': 'notification'},
+    {'wmclass': 'splash'},
+    {'wmclass': 'toolbar'},
+    {'wmclass': 'confirmreset'},  # gitk
+    {'wmclass': 'makebranch'},  # gitk
+    {'wmclass': 'maketag'},  # gitk
+    {'wname': 'branchdialog'},  # gitk
+    {'wname': 'pinentry'},  # GPG key password entry
+    {'wmclass': 'ssh-askpass'},  # ssh-askpass
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, github issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
