0cbc8f190d90a043981cad132b2b606c933a6d46
Roberto Solares <betosolaresgar@gmail.com>
2019-01-11 18:36:45 -0600

    Add a custom widget

---
+++ .config/qtile/customWidgets.py
@@ -0,0 +1,40 @@
+# 
+# Custom widget for qtile
+# 
+
+# Imports
+import os
+import subprocess
+from libqtile.widget import base
+
+# Widget to execute shell scripts
+class Script(base.ThreadedPollText):
+    orientations = base.ORIENTATION_HORIZONTAL
+    defaults = [
+        ('name', None, 'Script name'),
+        ('directory', os.path.expanduser('~/.scipts/'),
+    ]
+
+    def __init__(self, **config):
+        base.ThreadedPollText.__init__(self, **config)
+        self.add_defaults(Script.defaults)
+        self.name = self.directory + config['name']
+
+    def poll(self):
+        return self._run()
+
+    def _run(self, button = None, x = None, y = None):
+        if button is not None:
+            _button_ = os.environ.copy()
+            _button_['WIDGET_BUTTON'] = str(button)
+            _button_['WIDGET_X_LOC'] = str(x)
+            _button_['WIDGET_Y_LOC'] = str(y)
+            result = subprocess.run(self.name, stdout = subprocess.PIPE, env=_button_)
+        else:
+            result = subprocess.run(self.name, stadout=subprocess.PIPE)
+
+        return result.stdout.decode()
+
+    def button_press(self, x, y, button):
+        return self._run(button = button, x = x, y = y)
+
