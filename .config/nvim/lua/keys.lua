97eee7da1e15637bae39f4da33e73235f366a68f
Roberto Solares <betosolaresgar@gmail.com>
2024-07-20 22:04:34 -0600

    refactor: new keymaps

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -3,8 +3,12 @@ vim.g.mapleader = ","
 local opts = { noremap = true, silent = true }
 
 -- scrolling
-vim.keymap.set("", "J", "<C-d>", opts)
-vim.keymap.set("", "K", "<C-u>", opts)
+vim.keymap.set("", "J", "<C-d>zz", opts)
+vim.keymap.set("", "K", "<C-u>zz", opts)
+
+-- move lines
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- file saving
 vim.keymap.set("c", "W", "w", opts)
@@ -12,16 +16,20 @@ vim.keymap.set("c", "Q", "q", opts)
 vim.keymap.set("n", "<leader>q", ":qall!<CR>", opts)
 
 -- splits
-vim.keymap.set("n", "<leader>cs", ":sp<CR>", opts)
-vim.keymap.set("n", "<leader>vs", ":vsp<CR>", opts)
-vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
-vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
-vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
-vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
-vim.keymap.set("n", "<C-q>h", "<C-w>H", opts)
-vim.keymap.set("n", "<C-q>j", "<C-w>J", opts)
-vim.keymap.set("n", "<C-q>k", "<C-w>K", opts)
-vim.keymap.set("n", "<C-q>l", "<C-w>L", opts)
+vim.keymap.set("n", "ss", ":sp<CR>", opts)
+vim.keymap.set("n", "sv", ":vsp<CR>", opts)
+
+-- change window
+vim.keymap.set("n", "sh", "<C-w>h", opts)
+vim.keymap.set("n", "sj", "<C-w>j", opts)
+vim.keymap.set("n", "sk", "<C-w>k", opts)
+vim.keymap.set("n", "sl", "<C-w>l", opts)
+
+-- move window
+vim.keymap.set("n", "<C-h>", "<C-w>H", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>J", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>K", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>L", opts)
 
 -- indentation
 vim.keymap.set("v", ">", ">gv", opts)
